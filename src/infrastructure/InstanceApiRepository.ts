83c53300de2959f07acdecbe2a3bdd36ff3cfa0a
Xavier Terés <xavier.teres@somenergia.coop>
2023-03-24 10:23:28 +0100

    Add instance API repository adapter

---
+++ src/infrastructure/InstanceApiRepository.ts
@@ -0,0 +1,21 @@
+import {InstanceRepository} from "@/domain/InstanceRepository"
+import {Instance} from "@/domain/Instance"
+
+export class InstanceApiRepository implements InstanceRepository {
+    async search(): Promise<Instance[]> {
+        return fetch(process.env.api + '/instances/')
+            .then((res) => res.json())
+            .then((data) => {
+                return data.map((item: any) => {
+                    return {
+                        name: item.name,
+                        repository: item.git_info.repository,
+                        commit: item.git_info.commit,
+                        branch: item.git_info.branch,
+                        pull_request: item.git_info.pull_request,
+                        connectionUrl: item.connection,
+                    };
+                })
+            });
+    }
+}
