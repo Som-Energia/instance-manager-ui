fa6802f936bae2a10d09a8e9bc49660b88e25904
Xavier Terés <xavier.teres@somenergia.coop>
2023-03-25 16:52:19 +0100

    Add dialog to start new instance

--- src/components/InstanceDashboard.tsx
+++ src/components/InstanceDashboard.tsx
@@ -1,7 +1,15 @@
 import {Box, Button, Container, Divider, Typography} from "@mui/material";
 import InstanceWidgetList from "@/components/InstanceWidgetList";
+import CreateInstanceDialog from "@/components/dialogs/CreateInstanceDialog";
+import {useState} from "react";
 
 export default function InstanceDashboard() {
+    const [createDialogOpen, setCreateDialogOpen] = useState(false);
+
+    const handleCreateDialogOpen = () => {
+        setCreateDialogOpen(true);
+    };
+
     const styles = {
         root: {
             display: 'flex',
@@ -22,12 +30,16 @@ export default function InstanceDashboard() {
         <Container sx={{my: 5}}>
             <Box style={styles.root}>
                 <Typography variant="h3" style={styles.text}>Current instances</Typography>
-                <Button variant="contained" color="primary">New instance</Button>
+                <Button variant="contained" onClick={handleCreateDialogOpen} color="primary">New instance</Button>
                 <Divider/>
             </Box>
             <Container sx={{my: 5}}>
                 <InstanceWidgetList></InstanceWidgetList>
             </Container>
+            <CreateInstanceDialog
+                createDialogOpen={createDialogOpen}
+                setCreateDialogOpen={setCreateDialogOpen}
+            />
         </Container>
     )
 }
---
+++ src/components/dialogs/CreateInstanceDialog.tsx
@@ -0,0 +1,99 @@
+import {
+    Alert,
+    Button,
+    Dialog,
+    DialogActions,
+    DialogContent,
+    DialogTitle,
+    Snackbar,
+    TextField,
+    Typography
+} from "@mui/material";
+import {InstanceCreatePullRequest} from "@/domain/InstanceCreatePullRequest";
+import React, {useState} from "react";
+import {InstanceApiRepository} from "@/infrastructure/InstanceApiRepository";
+
+export default function CreateInstanceDialog
+({
+     createDialogOpen,
+     setCreateDialogOpen,
+ }: {
+    createDialogOpen: boolean,
+    setCreateDialogOpen: Function,
+}) {
+
+    const [repository, setRepository] = useState('');
+    const [errorMessage, setErrorMessage] = useState(false);
+    const [successMessage, setSuccessMessage] = useState(false);
+    const [pullRequest, setPullRequest] = useState(0);
+
+    const handleClose = () => {
+        setCreateDialogOpen(false);
+    };
+
+    const handleSubmit = () => {
+        const instance: InstanceCreatePullRequest = {
+            repository: repository,
+            pullRequest: pullRequest,
+        };
+        new InstanceApiRepository()
+            .createPullRequest(instance)
+            .then(response => {
+                setErrorMessage(!response)
+                if (response) {
+                    handleClose();
+                    setSuccessMessage(true);
+                }
+            })
+    }
+
+    return (
+        <>
+            <Dialog
+                open={createDialogOpen}
+                onClose={handleClose}
+            >
+                <DialogTitle>
+                    Start new instance
+                </DialogTitle>
+                <DialogContent>
+                    <TextField
+                        autoFocus
+                        margin="dense"
+                        id="repository"
+                        label="Repository"
+                        type="text"
+                        fullWidth
+                        onChange={(event) => setRepository(event.target.value)}
+                    />
+                    <TextField
+                        autoFocus
+                        margin="dense"
+                        id="pullRequest"
+                        label="Pull request"
+                        type="number"
+                        fullWidth
+                        onChange={(event) => setPullRequest(Number(event.target.value))}
+                    />
+                    <Typography color="error" sx={{mt: 1.5}} hidden={!errorMessage}>
+                        Oops! Something went wrong. Please check the input and try again.
+                    </Typography>
+                </DialogContent>
+                <DialogActions>
+                    <Button onClick={handleClose}>CANCEL</Button>
+                    <Button onClick={handleSubmit}>START</Button>
+                </DialogActions>
+            </Dialog>
+            <Snackbar
+                open={successMessage}
+                autoHideDuration={5000}
+                onClose={() => {
+                    setSuccessMessage(false)
+                }}
+            >
+                <Alert severity="success">New instance is starting</Alert>
+            </Snackbar>
+
+        </>
+    );
+}
