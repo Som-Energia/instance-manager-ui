6d0e251ef98e92eacfdb6f94ebea7c7ac469fe76
Xavier Terés <xavier.teres@somenergia.coop>
2023-04-05 23:43:44 +0200

    Rename, add typing and add dynamically resize TerminalComponent

---
+++ src/components/TerminalComponent.tsx
@@ -0,0 +1,64 @@
+import React, {useEffect, useRef} from "react";
+import {Terminal} from "xterm";
+import {FitAddon} from "xterm-addon-fit";
+import {AttachAddon} from "xterm-addon-attach";
+import "xterm/css/xterm.css";
+
+export default function TerminalComponent({instanceName}: { instanceName: string }) {
+    const termRef = useRef<HTMLDivElement>(null);
+    const socketRef = useRef<WebSocket | null>(null);
+    const fitAddon = useRef<FitAddon>(new FitAddon());
+    const terminalRef = useRef<Terminal | null>(null);
+
+    useEffect(() => {
+        const terminal = new Terminal();
+        terminal.open(termRef.current!);
+        terminal.loadAddon(fitAddon.current);
+
+        socketRef.current = new WebSocket(`ws://localhost:8000/api/instances/${instanceName}/ssh`);
+
+        socketRef.current.onopen = () => {
+            console.log("Connected to SSH");
+            const attachAddon = new AttachAddon(socketRef.current!);
+            terminal.loadAddon(attachAddon);
+            fitAddon.current.fit();
+            sendResizeInfo();
+            window.addEventListener("resize", handleResize);
+        };
+
+        terminalRef.current = terminal;
+
+        return () => {
+            terminal.dispose();
+            socketRef.current?.close();
+            window.removeEventListener("resize", handleResize);
+        };
+    }, [instanceName]);
+
+    const handleResize = () => {
+        fitAddon.current.fit();
+        sendResizeInfo();
+    };
+
+    const sendResizeInfo = () => {
+        const rows = Math.floor(window.innerHeight / 17);
+        const cols = terminalRef.current!.cols;
+        terminalRef.current!.resize(cols, rows);
+        if (socketRef.current?.readyState === WebSocket.OPEN) {
+            socketRef.current?.send(JSON.stringify({type: "resize", rows, cols}));
+        }
+    };
+
+    const handleKeydown = (event: React.KeyboardEvent<HTMLDivElement>) => {
+        if (socketRef.current?.readyState === WebSocket.OPEN) {
+            socketRef.current?.send(JSON.stringify({type: "input", data: event.key}));
+        }
+    };
+
+    return (
+        <div className="terminal-container">
+            <div className="terminal" ref={termRef}></div>
+            <div tabIndex={0} onKeyDown={handleKeydown}></div>
+        </div>
+    );
+};
