10de83f3984a08ed05422731b056d9536efc9c8a
Xavier Terés <xavier.teres@somenergia.coop>
2023-04-06 18:55:59 +0200

    Add new snackbars and improve current ones

--- src/components/InstanceWidgetList.tsx
+++ src/components/InstanceWidgetList.tsx
@@ -1,4 +1,4 @@
-import {Grid} from "@mui/material";
+import {Alert, Grid, Snackbar} from "@mui/material";
 import InstanceWidget from "@/components/InstanceWidget"
 import useSWR from 'swr';
 import {Instance} from "@/domain/Instance";
@@ -8,31 +8,53 @@ import React, {useState} from "react";
 export default function InstanceWidgetList() {
     const {data, error} = useSWR<Instance[]>('instances', readInstances, {});
 
-    const instanceRepository = new InstanceApiRepository();
-    const [instances, setInstances] = useState<Instance[]>([])
-
-    useEffect(() => {
-        const fetchData = async () => {
-            const data = await instanceRepository.search();
-            setInstances(data);
-        };
-
-        fetchData();
-
-        const intervalId = setInterval(() => {
-            fetchData();
-        }, 10000);
-
-        return () => clearInterval(intervalId);
-    }, []);
+    const [successDeleteInstanceMessage, setSuccessDeleteInstanceMessage] = useState(false);
+    const [successCopyMessage, setSuccessCopyMessage] = useState(false);
 
     return (
-        <Grid container spacing={5}>
-            {instances.map((instance) => (
-                <Grid item xs={12} sm={12} md={12} lg={6} key={instance.name}>
-                    <InstanceWidget instance={instance}></InstanceWidget>
-                </Grid>
-            ))}
-        </Grid>
+        <>
+            <Grid container spacing={5}>
+                {data?.map((instance) => (
+                    <Grid item xs={12} sm={12} md={12} lg={6} key={instance.name}>
+                        <InstanceWidget
+                            instance={instance}
+                            setSuccessDeleteInstanceMessage={setSuccessDeleteInstanceMessage}
+                            setSuccessCopyMessage={setSuccessCopyMessage}
+                        />
+                    </Grid>
+                ))}
+            </Grid>
+
+            {/* Error message fetching instances from API */}
+            {error &&
+                <Snackbar open={error}>
+                    <Alert variant="filled" severity="error" sx={{width: '100%'}}>
+                        Cannot get instances: {error.message}
+                    </Alert>
+                </Snackbar>
+            }
+
+            {/* Success instance deletion message */}
+            <Snackbar
+                open={successDeleteInstanceMessage}
+                autoHideDuration={3000}
+                onClose={() => {
+                    setSuccessDeleteInstanceMessage(false)
+                }}
+            >
+                <Alert variant="filled" severity="success">Successfully deleted instance</Alert>
+            </Snackbar>
+
+            {/* Success copy text to clipboard message */}
+            <Snackbar
+                open={successCopyMessage}
+                autoHideDuration={1000}
+                onClose={() => {
+                    setSuccessCopyMessage(false)
+                }}
+            >
+                <Alert variant="filled" severity="success">Successfully copied to your clipboard</Alert>
+            </Snackbar>
+        </>
     );
 };
