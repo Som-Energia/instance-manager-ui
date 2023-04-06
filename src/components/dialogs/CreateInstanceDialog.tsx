cfd81906231a48dc2782bcbed73b5e809770058e
Xavier Terés <xavier.teres@somenergia.coop>
2023-04-06 18:58:42 +0200

    Refetch instances on create instance and minor fixes to Snackbar

--- src/components/dialogs/CreateInstanceDialog.tsx
+++ src/components/dialogs/CreateInstanceDialog.tsx
@@ -9,9 +9,9 @@ import {
     TextField,
     Typography
 } from "@mui/material";
-import {InstanceCreatePullRequest} from "@/domain/InstanceCreatePullRequest";
 import React, {useState} from "react";
-import {InstanceApiRepository} from "@/infrastructure/InstanceApiRepository";
+import {mutate} from "swr";
+import {createInstanceFromPullRequest} from "@/services/api";
 
 export default function CreateInstanceDialog
 ({
@@ -23,30 +23,26 @@ export default function CreateInstanceDialog
 }) {
 
     const [repository, setRepository] = useState('');
-    const [errorMessage, setErrorMessage] = useState(false);
-    const [successMessage, setSuccessMessage] = useState(false);
     const [pullRequest, setPullRequest] = useState(0);
+
+    const [errorMessage, setErrorMessage] = useState('');
+    const [successCreateInstanceMessage, setSuccessCreateInstanceMessage] = useState(false);
 
     const handleClose = () => {
         setCreateDialogOpen(false);
-        setErrorMessage(false);
+        setErrorMessage('');
     };
 
-    const handleSubmit = () => {
-        const instance: InstanceCreatePullRequest = {
-            repository: repository,
-            pullRequest: pullRequest,
-        };
-        new InstanceApiRepository()
-            .createPullRequest(instance)
-            .then(response => {
-                setErrorMessage(!response)
-                if (response) {
-                    handleClose();
-                    setSuccessMessage(true);
-                }
-            })
-    }
+    const handleCreate = async () => {
+        try {
+            await createInstanceFromPullRequest(repository, pullRequest);
+            setSuccessCreateInstanceMessage(true);
+            handleClose();
+            mutate('instances');
+        } catch (error: any) {
+            setErrorMessage((error as Error).message)
+        }
+    };
 
     return (
         <>
@@ -77,22 +73,24 @@ export default function CreateInstanceDialog
                         onChange={(event) => setPullRequest(Number(event.target.value))}
                     />
                     <Typography color="error" sx={{mt: 1.5}} hidden={!errorMessage}>
-                        Oops! Something went wrong. Please check the input and try again.
+                        {errorMessage}
                     </Typography>
                 </DialogContent>
                 <DialogActions>
                     <Button onClick={handleClose}>CANCEL</Button>
-                    <Button onClick={handleSubmit}>START</Button>
+                    <Button onClick={handleCreate}>START</Button>
                 </DialogActions>
             </Dialog>
+
+            {/* Success instance create message */}
             <Snackbar
-                open={successMessage}
-                autoHideDuration={5000}
+                open={successCreateInstanceMessage}
+                autoHideDuration={3000}
                 onClose={() => {
-                    setSuccessMessage(false)
+                    setSuccessCreateInstanceMessage(false)
                 }}
             >
-                <Alert severity="success">New instance is starting</Alert>
+                <Alert variant="filled" severity="success">New instance is starting</Alert>
             </Snackbar>
         </>
     );
