0d4946a964625d23da677b902bdeefa4457d7db9
Xavier Terés <xavier.teres@somenergia.coop>
2023-04-06 19:00:37 +0200

    Minor fixes to use new snackbar and refetch instances on successfully delete instance

--- src/components/dialogs/DeleteInstanceDialog.tsx
+++ src/components/dialogs/DeleteInstanceDialog.tsx
@@ -1,35 +1,41 @@
-import {Button, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle} from "@mui/material";
-import {useEffect} from "react";
-import {InstanceApiRepository} from "@/infrastructure/InstanceApiRepository";
+import {Button, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle, Typography} from "@mui/material";
 import {Instance} from "@/domain/Instance";
+import {deleteInstance} from "@/services/api";
+import {mutate} from "swr";
+import React, {useState} from "react";
 
 export default function DeleteInstanceDialog
 ({
      instance,
      deleteDialogOpen,
      setDeleteDialogOpen,
-     instanceDelete,
-     setInstanceDelete
+     setInstanceDelete,
+     setSuccessDeleteInstanceMessage
  }: {
     instance: Instance,
     deleteDialogOpen: boolean,
     setDeleteDialogOpen: Function,
-    instanceDelete: boolean, setInstanceDelete: Function
+    setInstanceDelete: Function,
+    setSuccessDeleteInstanceMessage: Function,
 }) {
 
-    useEffect(() => {
-        if (instanceDelete) {
-            new InstanceApiRepository().delete(instance);
-        }
-    }, [instanceDelete]);
+    const [errorMessage, setErrorMessage] = useState('');
 
     const handleClose = () => {
         setDeleteDialogOpen(false);
+        setErrorMessage('');
     };
 
-    const handleDelete = () => {
-        setDeleteDialogOpen(false);
-        setInstanceDelete(true);
+    const handleDelete = async () => {
+        try {
+            await deleteInstance(instance.name);
+            setInstanceDelete(true);
+            setSuccessDeleteInstanceMessage(true);
+            mutate('instances');
+            handleClose();
+        } catch (error: any) {
+            setErrorMessage((error as Error).message)
+        }
     };
 
     return (
@@ -45,6 +51,9 @@ export default function DeleteInstanceDialog
                     Deleting an instance will result in the permanent removal of all associated data, and this action
                     cannot be undone.
                 </DialogContentText>
+                <Typography color="error" sx={{mt: 1.5}} hidden={!errorMessage}>
+                    {errorMessage}
+                </Typography>
             </DialogContent>
             <DialogActions>
                 <Button onClick={handleClose}>CANCEL</Button>
