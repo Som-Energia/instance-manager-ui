a23743f3b151dcc30d744ef5fb70769f658b573b
Xavier Terés <xavier.teres@somenergia.coop>
2023-04-07 12:30:05 +0200

    Add dropdown menu with create new instance options

--- src/components/InstanceDashboard.tsx
+++ src/components/InstanceDashboard.tsx
@@ -1,21 +1,10 @@
-import {Box, Button, Container, Divider, Typography} from "@mui/material";
+import {Box, Button, Container, Divider, Menu, MenuItem, Typography} from "@mui/material";
 import InstanceWidgetList from "@/components/InstanceWidgetList";
 import CreatePullRequestInstanceDialog from "@/components/dialogs/CreatePullRequestInstanceDialog";
-import {useState} from "react";
+import {MouseEvent, useState} from "react";
 import CreateBranchInstanceDialog from "@/components/dialogs/CreateBranchInstanceDialog";
 
 export default function InstanceDashboard() {
-    const [createBranchInstanceDialogOpen, setCreateBranchInstanceDialogOpen] = useState(false);
-    const [createPullRequestInstanceDialogOpen, setCreatePullRequestInstanceDialogOpen] = useState(false);
-
-    const handlePullRequestInstanceCreateDialogOpen = () => {
-        setCreatePullRequestInstanceDialogOpen(true);
-    };
-
-    const handleBranchInstanceCreateDialogOpen = () => {
-        setCreateBranchInstanceDialogOpen(true);
-    };
-
     const styles = {
         root: {
             display: 'flex',
@@ -32,21 +21,67 @@ export default function InstanceDashboard() {
         },
     };
 
+    const [createBranchInstanceDialogOpen, setCreateBranchInstanceDialogOpen] = useState(false);
+    const [createPullRequestInstanceDialogOpen, setCreatePullRequestInstanceDialogOpen] = useState(false);
+
+    const handlePullRequestInstanceCreateDialogOpen = () => {
+        setCreatePullRequestInstanceDialogOpen(true);
+    };
+
+    const handleBranchInstanceCreateDialogOpen = () => {
+        setCreateBranchInstanceDialogOpen(true);
+    };
+
+    // Create instance dropdown menu
+
+    const [anchorEl, setAnchorEl] = useState<EventTarget | null>(null);
+
+    const handleButtonClick = (event: MouseEvent<HTMLButtonElement>) => {
+        setAnchorEl(event.currentTarget);
+    };
+
+    const handleMenuItemClick = (event: MouseEvent<HTMLLIElement>, option: string) => {
+        switch (option) {
+            case 'branch':
+                handleBranchInstanceCreateDialogOpen();
+                break;
+            case 'pull_request':
+                handlePullRequestInstanceCreateDialogOpen();
+                break;
+            default:
+                break;
+        }
+        setAnchorEl(null);
+    };
+
+    const handleMenuClose = () => {
+        setAnchorEl(null);
+    };
+
     return (
         <Container sx={{my: 5}}>
             <Box style={styles.root}>
                 <Typography variant="h4" style={styles.text}>Current instances</Typography>
                 <Button
                     variant="contained"
-                    onClick={handleBranchInstanceCreateDialogOpen}
+                    onClick={handleButtonClick}
                     color="primary"
-                    sx={{mr: 1}}
                 >
-                    New from branch
+                    New instance
                 </Button>
-                <Button variant="contained" onClick={handlePullRequestInstanceCreateDialogOpen} color="primary">
-                    New from pull request
-                </Button>
+                <Menu
+                    id="new-instance-menu"
+                    anchorEl={anchorEl as HTMLElement}
+                    open={Boolean(anchorEl)}
+                    onClose={handleMenuClose}
+                >
+                    <MenuItem onClick={(event) => handleMenuItemClick(event, 'branch')}>
+                        From branch
+                    </MenuItem>
+                    <MenuItem onClick={(event) => handleMenuItemClick(event, 'pull_request')}>
+                        From pull request
+                    </MenuItem>
+                </Menu>
                 <Divider/>
             </Box>
             <Container sx={{my: 5}}>
